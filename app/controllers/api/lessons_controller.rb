d071cde2730b4edc8b776499ddc6b740783d23b9
dreamlx <dreamlinx@gmail.com>
2014-01-16 11:44:20 +0800

    build lessons api

--- app/controllers/api/lessons_controller.rb
+++ app/controllers/api/lessons_controller.rb
@@ -2,22 +2,41 @@ module Api
   class LessonsController < Api::BaseController
     def index
       #TODO : 查询对应日期课程和可用状态
-      #@lessondate = params[:lessondate]
-      #@venue = Venue.find(params[:venue_id])
-      if params[:venue_id].blank?
-        lessons = Lesson.all
-      else
-        lessons = Venue.find(params[:venue_id]).lessons
-      end
-      @lessons = []
+      current_date = params[:lessondate].to_date.beginning_of_week
+        
+      @lesson_groupby_dates = []
 
+      # 获取2周
       (0..13).each do |i|
-        if lessons[i].nil? 
-          @lessons << Lesson.new 
-        else
-          @lessons << lessons[i]
+        item = {lesson_date: current_date}
+        current_date = current_date.next_day
+        tmp_lessons = Venue.find(params[:venue_id]).lessons
+        lessons = []
+        tmp_lessons.each do |lesson|
+          #课程id、课程时间、老师id、老师照片、老师姓名、课程名称、教室名称、预订状态
+          new_lesson = { id: lesson.id, 
+                         starting_time: lesson.starting_time, 
+                         ending_time: lesson.ending_time,
+                         course: { id: lesson.course_id, name: lesson.course.name},
+                         room: { id: lesson.room_id, name: lesson.room.name},
+                         state: "TODO"
+                       }
+          
+          if lesson.teacher.nil?
+            teacher = {}
+          else
+            teacher =  { id: lesson.teacher_id, name: lesson.teacher.name, img: lesson.teacher.img}
+          end
+          new_lesson[:teacher] = teacher
+
+          lessons << new_lesson
         end
+        
+        item[:lessons] = lessons
+        @lesson_groupby_dates << item
+        
       end
+       render json: {lessons: @lesson_groupby_dates}
     end
 
     def show
